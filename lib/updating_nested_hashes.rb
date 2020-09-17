fcb06b31b79fac3c1aac9264a2e667b4ad7b19cd
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-17 21:38:58 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -3,7 +3,7 @@ def update_status
        :montague => {
           :patriarch => {name: "Lord Montague", age: "53"},
           :matriarch => {name: "Lady Montague", age: "54"},
-          :hero => {name: "Romeo", age: "15", status: "dead"},
+          :hero => {name: "Romeo", age: "15", status: "alive"},
           :hero_friends => [
              {name: "Benvolio", age: "17", attitude: "worried"},
              {name: "Mercutio", age: "18", attitude: "hot-headed"}
@@ -12,7 +12,7 @@ def update_status
        :capulet => {
           :patriarch => {name: "Lord Capulet", age: "50"},
           :matriarch => {name: "Lady Capulet", age: "51"},
-          :heroine => {name: "Juliet", age: "15", status: "dead"},
+          :heroine => {name: "Juliet", age: "15", status: "alive"},
           :heroine_friends => [
           {name: "Steven", age: "30", attitude: "confused"},
           {name: "Nurse", age: "44", attitude: "worried"}
